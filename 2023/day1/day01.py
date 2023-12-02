bd7747734bcefb4d1308cd8da31ad22e91b86abd
darryl-ad <91731499+darryl-ad@users.noreply.github.com>
2023-12-02 15:32:02 +0000

    ruff formatting

--- 2023/day1/day01.py
+++ 2023/day1/day01.py
@@ -1,64 +1,68 @@
 import os
 import time
+
 path = os.path.dirname(__file__) + "\input.txt"
 
 start = time.perf_counter()
 
-def get_ordered_map():   
+
+def get_ordered_map():
     num_map = {
         "one": 1,
         "two": 2,
         "three": 3,
         "four": 4,
-        "five":5,
-        "six":6,
-        "seven":7,
-        "eight":8,
-        "nine":9,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
     }
     order = sorted(num_map.keys(), key=len)
     ordered_map = {k: num_map[k] for k in order}
     return ordered_map
 
+
 def first_digit(line, map):
     for i in range(len(line)):
         if line[i].isdigit():
             return line[i]
-        
+
         for k in map.keys():
             if line[i:].startswith(k):
                 return str(map[k])
-            
+
+
 def last_digit(line, map):
     for i in reversed(range(len(line))):
         if line[i].isdigit():
             return line[i]
-        
+
         for k in map.keys():
-            if line[:i+1].endswith(k):
-                return str(map[k])        
+            if line[: i + 1].endswith(k):
+                return str(map[k])
+
 
 def solution(path):
     with open(path) as f:
         data = f.read()
-        
+
     map = get_ordered_map()
     sum1 = 0
     sum2 = 0
-    
+
     for line in data.splitlines():
         digits1 = [s for s in list(line) if s.isdigit()]
-        digits2 = [first_digit(line,map), last_digit(line,map)]
-        
+        digits2 = [first_digit(line, map), last_digit(line, map)]
+
         sum1 += int(digits1[0] + digits1[-1])
         sum2 += int(digits2[0] + digits2[-1])
 
     return [sum1, sum2]
+
 
 sol = solution(path)
 
 end = time.perf_counter()
 print(f"Solution = {sol}")
 print(f"Time = {(end-start)*1000:.3f} ms")
-
-    
