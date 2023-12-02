53eb40da6bf361c8225861bf2eaa4c6b0b68305e
darryl-ad <91731499+darryl-ad@users.noreply.github.com>
2023-12-02 15:55:17 +0000

    update boilerplate

--- 2023/boilerplate.py
+++ 2023/boilerplate.py
@@ -1,13 +1,18 @@
 import os
 import time
-path = os.path.dirname(__file__) + "\input.txt"
+
+example = True
+if example:
+    path = os.path.dirname(__file__) + "\example.txt"
+else:
+    path = os.path.dirname(__file__) + "\input.txt"
 start = time.perf_counter()
 
 
 
 
-end = time.perf_counter()
-print(f"Solution = {sol}")
-print(f"Time = {(end-start)*1000:.3f} ms")
+# end = time.perf_counter()
+# print(f"Solution = {sol}")
+# print(f"Time = {(end-start)*1000:.3f} ms")
 
     
