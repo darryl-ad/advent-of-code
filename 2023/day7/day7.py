f6f73322101b518a4dfc880938e8042987580918
darryl-ad <91731499+darryl-ad@users.noreply.github.com>
2023-12-09 20:01:51 +0000

    formatting

--- 2023/day7/day7.py
+++ 2023/day7/day7.py
@@ -14,14 +14,15 @@ start = time.perf_counter()
 from collections import Counter
 from itertools import groupby
 
+
 def replace_jokers(hand):
-# Takes the hand with face cards already converted to values
+    # Takes the hand with face cards already converted to values
     J_val = facecards["J"]
     if J_val not in hand:
         return hand
     else:
         # Remove jokers first so we don't replace like for like!
-        no_jokers = [c for c in hand if c!=J_val]
+        no_jokers = [c for c in hand if c != J_val]
         if len(set(hand)) == 1:
             # Hand is all jokers
             new_val = facecards["A"]
@@ -30,11 +31,12 @@ def replace_jokers(hand):
             new_val = max(no_jokers)
         else:
             # Find the most common card, taking the most valuable if there is a tie
-            freqs = groupby(Counter(no_jokers).most_common(), lambda x:x[1])
-            new_val = max([val for val,count in next(freqs)[1]])
-            
-    new_hand = [c if c!=J_val else new_val for c in hand]
+            freqs = groupby(Counter(no_jokers).most_common(), lambda x: x[1])
+            new_val = max([val for val, count in next(freqs)[1]])
+
+    new_hand = [c if c != J_val else new_val for c in hand]
     return new_hand
+
 
 def cards_to_vals(card):
     if card in facecards:
@@ -42,27 +44,29 @@ def cards_to_vals(card):
     else:
         return int(card)
 
-def parse(data, jokers = False):
+
+def parse(data, jokers=False):
     hands = []
     bids = []
     strengths = []
     for line in data:
         hand, bid = line.split(" ")
         hand = [cards_to_vals(c) for c in hand]
-        
+
         if jokers:
             # Hand strength is based off the hand without jokers
             # But the jokers are still counted in ranking high cards in a tie, but with the lowest value
             hand_no_jokers = replace_jokers(hand)
-            strengths.append(get_hand_strength(hand_no_jokers)) 
-            hands.append([c if c!=facecards["J"] else 1 for c in hand])
+            strengths.append(get_hand_strength(hand_no_jokers))
+            hands.append([c if c != facecards["J"] else 1 for c in hand])
         else:
-            strengths.append(get_hand_strength(hand)) 
+            strengths.append(get_hand_strength(hand))
             hands.append(hand)
-            
+
         bids.append(int(bid))
-        
+
     return hands, bids, strengths
+
 
 def get_hand_strength(hand):
     n_uniq = len(set(hand))
@@ -70,14 +74,14 @@ def get_hand_strength(hand):
     if n_uniq == n:
         # High card
         return 1
-    elif n_uniq == n-1:
+    elif n_uniq == n - 1:
         # Pair
         return 2
-    elif n_uniq == n-2:
+    elif n_uniq == n - 2:
         # 2 Pair or 3 of a kind
         strength = 3 if max(Counter(hand).values()) == 2 else 4
         return strength
-    elif n_uniq == n-3:
+    elif n_uniq == n - 3:
         # Full house or 4 Kind
         strength = 5 if max(Counter(hand).values()) == 3 else 6
         return strength
@@ -85,23 +89,25 @@ def get_hand_strength(hand):
         # 5 of a kind
         return 7
 
-def calc_winnings(data, jokers = False):
+
+def calc_winnings(data, jokers=False):
     hands, bids, strengths = parse(data, jokers)
 
     # Sort bids by strength, then by cards in hands
     # This appears to be correctly sorting by 1st card then 2nd card
     # [bids, strengths, hands]
-    ranked =sorted(zip(bids, strengths, hands), key = lambda x: (x[1], x[2]))
+    ranked = sorted(zip(bids, strengths, hands), key=lambda x: (x[1], x[2]))
 
     winnings = 0
     for i, rank in enumerate(ranked):
-        winnings += (i+1) * rank[0]
-    
+        winnings += (i + 1) * rank[0]
+
     return winnings
+
 
 with open(path) as f:
     data = f.read().splitlines()
-    
+
 facecards = {
     "A": 14,
     "K": 13,
@@ -110,11 +116,9 @@ facecards = {
     "T": 10,
 }
 
-sol = (calc_winnings(data), calc_winnings(data, jokers = True))
+sol = (calc_winnings(data), calc_winnings(data, jokers=True))
 
 # OUTPUT
 end = time.perf_counter()
 print(f"Solution = {sol}")
 print(f"Time = {(end-start)*1000:.3f} ms")
-
-    
--- 2023/day8/day8.py
+++ 2023/day8/day8.py
@@ -12,8 +12,10 @@ start = time.perf_counter()
 
 # IMPLEMENTATION
 import math
+
 with open(path) as f:
     data = f.read().splitlines()
+
 
 def parse(data):
     instructs = data[0]
@@ -26,18 +28,20 @@ def parse(data):
         ptr = ptr.replace("(", "").replace(")", "").split(",")
         nodes.append(node)
         ptrs.append(ptr)
-        
+
     return instructs, nodes, ptrs
+
 
 def next_node(node, nodes, ptrs, instruction):
     idx = nodes.index(node)
-    
+
     if instruction == "L":
         return ptrs[idx][0]
     elif instruction == "R":
         return ptrs[idx][1]
     else:
         raise Exception("Invalid instruction")
+
 
 def get_A_nodes(nodes):
     A_nodes = []
@@ -46,10 +50,12 @@ def get_A_nodes(nodes):
             A_nodes.append(node)
     return A_nodes
 
+
 def check_if_Z_node(node):
     if node[-1] != "Z":
         return False
     return True
+
 
 def find_zzz(data):
     steps = 0
@@ -58,9 +64,10 @@ def find_zzz(data):
     while not node == "ZZZ":
         node = next_node(node, nodes, ptrs, instructs[i])
         steps += 1
-        i = i+1 if i < len(instructs)-1 else 0 # Instructions can repeat
-    
+        i = i + 1 if i < len(instructs) - 1 else 0  # Instructions can repeat
+
     return steps
+
 
 def find_xyz(data, start_node):
     steps = 0
@@ -69,22 +76,24 @@ def find_xyz(data, start_node):
     while not check_if_Z_node(node):
         node = next_node(node, nodes, ptrs, instructs[i])
         steps += 1
-        i = i+1 if i < len(instructs)-1 else 0 # Instructions can repeat
-    
+        i = i + 1 if i < len(instructs) - 1 else 0  # Instructions can repeat
+
     return steps
 
+
 def run_part2(data):
-# Find the steps for each __A node until __Z
-# Then assume that they loop at this period
-# The number where they sync is the lowest common mutliple of the periods
-# There is NOTHING in the question to suggest that this is the case - flawed question.
+    # Find the steps for each __A node until __Z
+    # Then assume that they loop at this period
+    # The number where they sync is the lowest common mutliple of the periods
+    # There is NOTHING in the question to suggest that this is the case - flawed question.
     ghost_nodes = get_A_nodes(nodes)
     steps = []
     for node in ghost_nodes:
         steps.append(find_xyz(data, node))
-    
+
     ans = math.lcm(*steps)
     return ans
+
 
 instructs, nodes, ptrs = parse(data)
 sol = (find_zzz(data), run_part2(data))
@@ -94,5 +103,3 @@ sol = (find_zzz(data), run_part2(data))
 end = time.perf_counter()
 print(f"Solution = {sol}")
 print(f"Time = {(end-start)*1000:.3f} ms")
-
-    
