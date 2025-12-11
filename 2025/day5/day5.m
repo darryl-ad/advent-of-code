16c07c4e937034adbd6188a0cb24815f44cc2fa7
darryl-ad <91731499+darryl-ad@users.noreply.github.com>
2025-12-11 23:40:11 +0000

    part 2 solution

--- 2025/day5/day5.m
+++ 2025/day5/day5.m
@@ -21,15 +21,68 @@ end
 
 ingredient_ids = str2double(ingredient_ids_str);
 
-%% Determine which ids are within a range
-% % First sort ranges by first id
-% [~, idx_sort] = sort(ingredient_ranges(:,1), 'ascend');
-% ingredient_ranges = ingredient_ranges(idx_sort, :);
-
+%% Determine which IDs are within a range
 is_fresh = NaN(n_ids, 1);
 for idx_ids = 1:n_ids
     id = ingredient_ids(idx_ids);
-    is_fresh(idx_ids) = any(id >= ingredient_ranges(:,1) & id <= ingredient_ranges(:,2));
+    is_fresh(idx_ids) = any(find_range_membership(id, ingredient_ranges(:,1), ingredient_ranges(:,2)));
 end
 
 fprintf("PART 1 = %i\n", sum(is_fresh))
+
+%% Determine all possible fresh IDs
+% First sort ranges by first id
+[~, idx_sort] = sort(ingredient_ranges(:,1), 'ascend');
+ingredient_ranges = ingredient_ranges(idx_sort, :);
+
+% Consolidate the ranges into non-overlapping ranges
+consolidated_ranges = ingredient_ranges(1, :);
+
+for idx_range = 2:n_ranges
+
+    id_start = ingredient_ranges(idx_range, 1);
+    id_end = ingredient_ranges(idx_range, 2);
+
+    % Check if id_start overlaps with any of the consolidated ranges
+    overlapped_start = find(find_range_membership(id_start, consolidated_ranges(:,1), consolidated_ranges(:,2)));
+
+    if isempty(overlapped_start)
+        % add a new range
+        consolidated_ranges = [consolidated_ranges; [id_start, id_end]]; % Accept changing array size, ~1000 rows
+
+    elseif length(overlapped_start) > 1
+        error("id_start overlaps with more than 1 consolidated range")
+
+    else
+        % Expand existing range?
+        if id_end <= consolidated_ranges(overlapped_start, 2)
+            % Range is already covered. Move on
+            continue
+        end
+
+        % Expand the consolidated range end point
+        consolidated_ranges(overlapped_start, 2) = id_end;
+
+        % Check updated consolidated range boundaries for overlap. 
+        for idx = overlapped_start : height(consolidated_ranges) - 1
+            if id_end >= consolidated_ranges(idx+1, 1)
+                
+                % Get new range end id
+                id_end = consolidated_ranges(idx+1, 2);
+
+                % Remove unneeded range
+                consolidated_ranges(idx+1, :) = [NaN, NaN];
+
+                % Update consolidated range
+                consolidated_ranges(overlapped_start, 2) = id_end;
+            end
+        end
+    end
+end
+
+fprintf("PART 2 = %i\n", sum(consolidated_ranges(:,2) - consolidated_ranges(:,1) + 1))
+
+function is_in_range = find_range_membership(id, ranges_start, ranges_end)
+% Find ranges which id is a member of
+    is_in_range = id >= ranges_start & id <= ranges_end;
+end
