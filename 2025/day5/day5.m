a0b83b8bfa48f37dedc9e4c908c11747301b6fd8
darryl-ad <91731499+darryl-ad@users.noreply.github.com>
2025-12-12 00:27:13 +0000

    var naming

--- 2025/day5/day5.m
+++ 2025/day5/day5.m
@@ -13,9 +13,9 @@ n_ids = height(ingredient_ids_str);
 
 %% Convert to numeric for easier downstream processing
 ingredient_ranges = NaN(n_ranges, 2);
-for idx_range = 1:n_ranges 
-    ingredient_ranges(idx_range, :) = str2double( ...
-        strsplit(ingredient_ranges_str(idx_range), "-") ...
+for idx_curr = 1:n_ranges 
+    ingredient_ranges(idx_curr, :) = str2double( ...
+        strsplit(ingredient_ranges_str(idx_curr), "-") ...
         );
 end
 
@@ -38,10 +38,10 @@ ingredient_ranges = ingredient_ranges(idx_sort, :);
 % Consolidate the ranges into non-overlapping ranges
 consolidated_ranges = ingredient_ranges(1, :);
 
-for idx_range = 2:n_ranges
+for idx_curr = 2:n_ranges
 
-    id_start = ingredient_ranges(idx_range, 1);
-    id_end = ingredient_ranges(idx_range, 2);
+    id_start = ingredient_ranges(idx_curr, 1);
+    id_end = ingredient_ranges(idx_curr, 2);
 
     % Check if id_start overlaps with any of the consolidated ranges
     overlapped_start = find(find_range_membership(id_start, consolidated_ranges(:,1), consolidated_ranges(:,2)));
@@ -82,6 +82,8 @@ end
 
 fprintf("PART 2 = %i\n", sum(consolidated_ranges(:,2) - consolidated_ranges(:,1) + 1))
 
+
+%%
 function is_in_range = find_range_membership(id, ranges_start, ranges_end)
 % Find ranges which id is a member of
     is_in_range = id >= ranges_start & id <= ranges_end;
